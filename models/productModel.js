9ed6c9ecc85ba7f4cfff7bd88ed498a6308916f1
Guilherme Louli <guilhermeloulisoares@gmail.com>
2022-04-20 16:37:42 -0300

    Fetching all products

--- models/productModel.js
+++ models/productModel.js
@@ -16,6 +16,20 @@ class Product {
       })
       .catch(err => console.log(err));
   }
+
+  static fetchAll() {
+    const db = getDb();
+    return db
+      .collection('products')
+      .find()
+      .toArray()
+      .then(products => {
+        return products;
+      })
+      .catch(err => {
+        console.log(err)
+      });
+  }
 }
 
 module.exports = Product;
